f633919217a9f28d5ade90d0837196828cca0455
avalos010 <luizavalos40@gmail.com>
2021-06-08 21:17:15 -0500

    completed crud

---
+++ controllers/roomControllers.js
@@ -0,0 +1,125 @@
+import Room from '../models/room';
+
+//Get All Rooms => /api/rooms
+const allRooms =async (req,res) => {
+
+    try {
+   const rooms = await Room.find();
+   res.status(200).json({
+       success:true,
+       count: rooms.length,
+       rooms
+   })
+}
+catch(error) {
+    res.status(400).json({
+        success: false,
+        error: error.message
+    })
+}
+}
+
+//Create a new room => /api/rooms
+const newRoom = async (req,res ) => {
+    try{
+    const room = await  Room.create(req.body);
+    res.status(200).json({
+        success:true,
+        room
+    })
+}
+catch(error) {
+    res.status(400).json({
+        success: false,
+        error: error.message
+    })
+}
+}
+
+//Get room details => /api/rooms/:id
+
+const getSingleRoom = async (req,res ) => {
+    try{
+    const room = await  Room.findById(req.query.id);
+        if(!room) {
+            res.status(400).json({
+                success: false,
+                error: 'Room not found with this ID'
+            })
+        }
+    res.status(200).json({
+        success:true,
+        room
+    })
+}
+catch(error) {
+    res.status(400).json({
+        success: false,
+        error: error.message
+    })
+}
+}
+
+//update room details => /api/rooms/:id
+const updateRoom = async (req,res ) => {
+    try{
+    let room = await  Room.findById(req.query.id);
+    if(!room) {
+        res.status(400).json({
+            success: false,
+            error: 'Room not found with this ID'
+        })
+    }
+
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+        new: true,
+        runValidators: true,
+        userFindAndModify: false
+    })
+
+    res.status(200).json({
+        success:true,
+        room
+    })
+}
+catch(error) {
+    res.status(400).json({
+        success: false,
+        error: error.message
+    })
+}
+}
+
+
+//Delete room  => /api/rooms/:id
+
+const deleteRoom = async (req,res ) => {
+    try{
+    const room = await  Room.findById(req.query.id);
+        if(!room) {
+            res.status(400).json({
+                success: false,
+                error: 'Room not found with this ID'
+            })
+        }
+        await room.remove()
+    res.status(200).json({
+        success:true,
+        message:'Room is deleted'
+    })
+}
+catch(error) {
+    res.status(400).json({
+        success: false,
+        error: error.message
+    })
+}
+}
+
+export {
+    allRooms,
+    newRoom,
+    getSingleRoom,
+    updateRoom,
+    deleteRoom
+}
